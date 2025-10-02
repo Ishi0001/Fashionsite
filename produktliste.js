0f359a0c9299db42b6b124b3f9334f1c5300375d
isabellablanco <ishi0001@stud.kea.dk>
2025-10-02 12:44:49 +0200

    add

--- produktliste.js
+++ produktliste.js
@@ -14,9 +14,9 @@ function showProduct(products) {
         <a href="produkt.html?id=${element.id}" class="box">
           <img src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp" alt="tshirt">
           <span class="discount">-34%</span>
-          <h1>Sahara Team India Fawner Round Neck Jersey</h1>
+          <h1>${element.productdisplayname}</h1>
           <p>Tshirts i Nike</p>
-          <p>DKK 1595,-</p>
+          <p>DKK ${element.price},-</p>
           <p class="read-more">Read More</p>
         </a>`;
   });
